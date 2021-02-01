6e4ca062d7ace09fe42f3e71b316bb8c8b8b8ccc
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2021-02-01 15:05:58 +0100

    Created custom webpack setup without Vue CLI allowing dev server auto reload/watch on code changes using electron ('npm run dev' script, Vue CLI 'npm run electron:serve' couldnt watch for code changes and needed full compilation)

--- webserver-gatherer-front/README.md
+++ webserver-gatherer-front/README.md
@@ -1,24 +1,31 @@
 # web-server-gatherer
 
 ## Project setup
+
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+### Compiles and hot-reloads for development using custom Weback setup
+
+Note: Hot reload is made possible on electron by a custom Webpack setup (see ['wepack.config.js'](./wepack.config.js) and corresponding 'dev' script in ['package.json'](./package.json))
+
 ```
-npm run serve
+npm run dev
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- webserver-gatherer-front/src/background.js
+++ webserver-gatherer-front/src/background.js
@@ -1,18 +1,19 @@
 "use strict";
 
-import { app, protocol, BrowserWindow } from "electron";
-import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
-import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+const electron = require("electron");
+//const builder = require("vue-cli-plugin-electron-builder/lib");
+const devtools = require("electron-devtools-installer");
+require("electron-reload")(__dirname);
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Scheme must be registered before the app is ready
-protocol.registerSchemesAsPrivileged([
+electron.protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true } }
 ]);
 
 async function createWindow() {
   // Create the browser window.
-  const win = new BrowserWindow({
+  const win = new electron.BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -30,35 +31,35 @@ async function createWindow() {
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
     if (!process.env.IS_TEST) win.webContents.openDevTools();
   } else {
-    createProtocol("app");
+    //builder.createProtocol("app");
     // Load the index.html when not in development
-    win.loadURL("app://./index.html");
+    win.loadURL(`file://${__dirname}/../dev_build/index.html`);
   }
 }
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
+electron.app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
-    app.quit();
+    electron.app.quit();
   }
 });
 
-app.on("activate", () => {
+electron.app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  if (electron.BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", async () => {
+electron.app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension(VUEJS_DEVTOOLS);
+      await devtools.default(devtools.VUEJS_DEVTOOLS);
     } catch (e) {
       console.error("Vue Devtools failed to install:", e.toString());
     }
@@ -71,12 +72,12 @@ if (isDevelopment) {
   if (process.platform === "win32") {
     process.on("message", data => {
       if (data === "graceful-exit") {
-        app.quit();
+        electron.app.quit();
       }
     });
   } else {
     process.on("SIGTERM", () => {
-      app.quit();
+      electron.app.quit();
     });
   }
 }
